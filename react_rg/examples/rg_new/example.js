f15d10408a37da6ac95ca8dff0b0f1d581a36dec
phillippa morland <phillippa.morland@researchgate.net>
2015-11-20 17:49:33 +0100

    additions for me to look at the weekend yey!

--- react_rg/examples/rg_new/example.js
+++ react_rg/examples/rg_new/example.js
@@ -1,27 +1,70 @@
 var Editor = React.createClass({
   render: function() {
     return (
-      <div>
         <Input value={this.props.data.text} />
+
+    );
+  }
+});
+
+
+var Input = React.createClass({
+  getInitialState: function() {
+    return {value: this.props.value,
+            header: this.props.value,
+            paragraph: this.props.value,
+            button: this.props.value};
+  },
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+    this.setState({header: event.target.value});
+    this.setState({paragraph: event.target.value});
+    this.setState({button: event.target.value});
+  },
+
+  render: function() {
+    return (
+      <div>
+      <Output value={this.state.value} handleChange={this.handleChange} />
+      <Output2 value={this.state.value} handleChange={this.handleChange} />
+      <Output3 value={this.state.value} handleChange={this.handleChange} />
+
       </div>
     );
   }
 });
 
-var Input = React.createClass({
-  getInitialState: function() {
-    return {value: this.props.value};
-  },
-  handleChange: function(event) {
-    this.setState({value: event.target.value});
-  },
+var Output = React.createClass({
   render: function() {
     return (
-      <input type="text" maxLength="20" value={this.state.value} onChange={this.handleChange} />
+      <div>
+      <input type="text" onChange={this.props.handleChange} />
+      <h2>{this.props.value}</h2>
+      </div>
     );
   }
 });
 
+var Output2 = React.createClass({
+  render: function(){
+    return (
+      <div>
+      <input type="text" onChange={this.props.handleChange} />
+      <h3>{this.props.value}</h3>
+      </div>
+    );
+  }
+});
+var Output3 = React.createClass({
+  render: function(){
+    return (
+      <div>
+      <input type="text" onChange={this.props.handleChange} />
+      <button> {this.props.value} </button>
+      </div>
+    );
+  }
+});
 var appData = {text: "hello"};
 
 ReactDOM.render(
