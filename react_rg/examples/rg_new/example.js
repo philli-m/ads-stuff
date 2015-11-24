689aa1207df6aeb953cb4d17a7fe76dbb271e295
Philli Morland <phillippamorland@gmail.com>
2015-11-24 21:37:47 +0100

    this is now working

--- react_rg/examples/rg_new/example.js
+++ react_rg/examples/rg_new/example.js
@@ -1,5 +1,7 @@
   var update = React.addons.update;
 
+
+
 var Editor = React.createClass({
   getInitialState: function() {
     return this.props.data;
@@ -26,50 +28,49 @@ var Editor = React.createClass({
             <div class="form-group">
               <label>Header</label><br></br>
               <InputText name="header" property="text" maxLength="30" value={this.state.header.text}  handleChange={this.handleChange} />
-              <label>Text Shadow</label>
-              <TextShadow name="header" type="checkbox" hasShadow={this.state.header.shadow} handleChange={this.handleChange}/>
             </div>
+
             <div class="form-group">
               <label>Description</label><br></br>
               <InputText name="paragraph" property="text" maxLength="230" value={this.state.paragraph.text} handleChange={this.handleChange} />
-              <label>Text Shadow</label>
-              <TextShadow name="headerShadow" type="checkbox" checked="checked" />
             </div>
+
             <div class="form-group">
               <label>Button</label><br></br>
               <InputText name="button" property="text" value={this.state.button.text} handleChange={this.handleChange} />
-              <label>Text Shadow</label>
-              <TextShadow name="headerShadow" type="checkbox" checked="checked" />
             </div>
+
             <div class="form-group">
               <label>Header Color</label><br></br>
               <InputColor name="header" property="color" value={this.state.header.color}  handleChange={this.handleChange} />
             </div>
+
             <div class="form-group">
               <label>Description Color</label><br></br>
-              <InputColor name="paragraph" property="color" value={this.state.paragraph.color}  handleChange={this.handleChange} />
+               <InputColor name="paragraph" property="color" value={this.state.paragraph.color}  handleChange={this.handleChange} />
             </div>
+
             <div class="form-group">
               <label>Button Text Color</label><br></br>
               <InputColor name="button" property="color" value={this.state.button.color}  handleChange={this.handleChange} />
             </div>
+
             <div class="form-group">
               <label>Ad Color</label><br></br>
               <InputColor name="adBackground" property="color" value={this.state.adBackground.color}  handleChange={this.handleChange} />
-              <label>Boarder</label>
-              <TextShadow name="headerShadow" type="checkbox" checked="checked" />
             </div>
             <div class="form-group">
               <label>Button Color</label><br></br>
               <InputColor name="buttonBackground" property="color" value={this.state.buttonBackground.color}  handleChange={this.handleChange} />
             </div>
           </div>
-
-        <div id="appOutput" name="adBackground"  style={{backgroundColor: this.state.adBackground.color}} style={{boarder: this.state.adBackground.boarder}}>
+        <div id="appOutput" name="adBackground"  style={{backgroundColor: this.state.adBackground.color}}>
+          <a href="%%CLICK_URL_UNESC%%%%DEST_URL_ESC%%" target="_blank" class="rg-ad" id="rg-ad">
           <img class="rg-ad-img" id="rg-ad-img" src="#" />
-          <h3 style={{color: this.state.header.color}} style={{textShadow: this.state.header.shadow}}>{this.state.header.text} </h3>
+          <h3 style={{color: this.state.header.color}} >{this.state.header.text} </h3>
           <p style={{color: this.state.paragraph.color}}>{this.state.paragraph.text}</p>
-          <button style={{color: this.state.button.color}} style={{backgroundColor: this.state.buttonBackground.color}}>{this.state.button.text}</button>
+          <button style={{color: this.state.button.color, backgroundColor: this.state.buttonBackground.color}}> {this.state.button.text} </button>
+          </a>
         </div>
       </div>
     );
@@ -96,35 +97,32 @@ var TextShadow = React.createClass({
   render: function() {
     return (
         <input name={this.props.name} type="checkbox" checked={this.props.property} data-property={this.props.property} onChange={this.props.handleChange}/>
-    )
+    );
   }
-})
+});
 
 var appData = {
   header: {
     text: "title",
     color: "black",
-    hasShadow: true,
-    shadow: "1px 1px 2px black"
+
   },
   paragraph: {
     text: "Description",
     color: "black",
-    hasShadow: true,
-    shadow: "1px 1px 2px black"
+
   },
   button: {
     text: "Button",
     color: "black",
-    hasShadow: true,
-    shadow: "1px 1px 2px black"
+
   },
   buttonBackground: {
     color: "blue"
   },
   adBackground: {
     color: "white",
-    boarder: "black 3px"
+    border: "black 3px"
   },
 
 };
@@ -132,3 +130,26 @@ var appData = {
 ReactDOM.render(
   <Editor data={appData} />, document.getElementById('app')
 );
+var App = React.createClass({
+
+    displayName: 'App',
+
+    onDrag: function(color, c){
+        COLOR = color
+        this.setState({})
+    },
+
+    render: function(){
+
+        return (
+            <div>
+                <ColorPicker value={COLOR} onDrag={this.onDrag} />
+                <div style={{background: COLOR, width: 100, height: 50, color: 'white'}}>
+                    {COLOR}
+                </div>
+            </div>
+        )
+    }
+})
+
+React.renderComponent(App(), document.body)
